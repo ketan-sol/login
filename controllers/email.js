1b844097ee6b161e58a92359c8ee2468b2d53b08
ketan-sol <89595019+ketan-sol@users.noreply.github.com>
2021-11-11 11:31:52 +0530

    Update email.js

--- controllers/email.js
+++ controllers/email.js
@@ -1,7 +1,7 @@
 const sgMail = require('@sendgrid/mail')
 const dotenv = require('dotenv')
 dotenv.config()
-sgMail.setApiKey("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")
+sgMail.setApiKey("process.env.API_KEY")
 
 const login_email =  () => {
     const msg = {
